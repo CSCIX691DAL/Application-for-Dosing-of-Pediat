7eb9d016d0e8bdb2be1452743f308418fce02612
Faiyaz Tanim <55768599+brownbyteXCVI@users.noreply.github.com>
2021-10-20 14:25:16 -0300

    added form cards

--- dosing_app/lib/medications/med_2.dart
+++ dosing_app/lib/medications/med_2.dart
@@ -1,23 +1,49 @@
-import 'package:flutter/material.dart';
+import 'dart:html';
 
+import 'package:flutter/material.dart';
 
 class Med2 extends StatelessWidget {
   const Med2({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Medication 2"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
+  Widget buildCard(cardTitle) {
+    return Card(
+      color: Colors.blueGrey[100],
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 7),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 30),
+        child: Column(
+          children: <Widget>[
+            Text(
+              cardTitle,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "[Enter Value]",
+                border: InputBorder.none,
+              ),
+            )
+          ],
         ),
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Medication 2"),
+        ),
+        backgroundColor: Colors.blueGrey[700], //page background color
+
+        body: Column(
+          children: <Widget>[
+            buildCard("Patient Weight (kg)"),
+            buildCard("Patient Weight (kg)"),
+            buildCard("Patient Weight (kg)"),
+            buildCard("Patient Weight (kg)"),
+            buildCard("Patient Weight (kg)"),
+          ],
+        ));
   }
 }
