828111327fc08df5e57648ec55c57fb7e693ba7a
Loren Wolfe <lorewolf686@ed.amdsb.ca>
2021-10-29 12:18:03 -0300

    Created page for med 10

---
+++ dosing_app/lib/medications/med_10.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class Med10 extends StatefulWidget {
+  Med10(
+      {Key? key,
+        required this.index,
+        required this.medications,
+        required this.favMedications})
+      : super(key: key);
+  dynamic index;
+  dynamic medications;
+  dynamic favMedications;
+
+  @override
+  _Med10State createState() => _Med10State();
+}
+
+class _Med10State extends State<Med10> {
+  @override
+  Widget build(BuildContext context) {
+    Map medication = widget.medications[widget.index];
+    bool isFavourited = widget.favMedications.contains(medication);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Itraconazole"),
+        actions: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      if (isFavourited) {
+                        widget.favMedications.remove(medication);
+                      } else {
+                        widget.favMedications.add(medication);
+                      }
+                    });
+                    print(widget.favMedications);
+                  },
+                  child: Icon(
+                    isFavourited
+                        ? Icons.bookmark
+                        : Icons.bookmark_outline_rounded,
+                    size: 34,
+                    // color: isFavourited ? Colors,
+                  )))
+        ],
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
